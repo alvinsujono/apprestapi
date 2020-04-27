92dd43bfa6a2153feafc35082c53d0eee928ad8c
Alvin <alvinsujono10@outlook.com>
2020-04-28 01:28:19 +0700

    berhasil menampilkan semua mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Aplikasi REST API ku berjalan!",res)
 };
+
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FORM mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
