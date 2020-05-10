052d2c7909a6f2dcd9e2e1114313d689d494fd94
Alvin <alvinsujono10@outlook.com>
2020-05-10 20:49:52 +0700

    berhasil menampilkan  data mahasiswa berdasarkan id

--- routes.js
+++ routes.js
@@ -8,6 +8,5 @@ module.exports = function(app){
     //
     app.route('/tampil')
     .get(jsonku.tampilsemuamahasiswa); 
-    app.route('/tampil/:id')
-        .get(jsonku.tampilberdasarkanid);
+   
 }
